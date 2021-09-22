f1d7f5da63f5fcf56bf6a3d14ac60cd4ae1ebae0
Moeketsi17 <89091903+Moeketsi17@users.noreply.github.com>
2021-09-22 15:12:58 +0200

    Add files via upload

---
+++ loopstudios landing page/script.js
@@ -0,0 +1,13 @@
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', ()=> {
+    // Toggle nav
+    navLinks.classList.toggle('nav-active');
+
+});
+
+
+
+
+
